0fd324fe79b15087afa4d01e1eb8b7a2607a4809
CauanTeixeira <cauan.jesus.ct@outlook.com>
2022-11-08 15:32:20 -0300

    commit inicial

---
+++ POO/control.php
@@ -0,0 +1,38 @@
+<?php
+$control=[
+    'usuario1' => [
+        'User' => 'silas@email.com',
+        'pass' => '1234',
+        'name' => 'Silas Kando',
+        'cpf' => '504.572.627-70',
+        'img' => 'imagem/user1.png'
+    ],
+    'usuario2' => [
+        'User' => 'catabinvbas@outlook.com',
+        'pass' => '546',
+        'name' => 'gelado oficial',
+        'cpf' => '423.775.458-82',
+        'img' => 'imagem/user2.png'
+    ],
+    'usuario3' => [
+        'User' => 'jujutsu@gmail.com',
+        'pass' => '3546',
+        'name' => 'churras',
+        'cpf' => '590.686.981-68',
+        'img' => 'imagem/user3.png'
+    ],
+    'usuario4' => [
+        'User' => 'Vivo@mail.com',
+        'pass' => '656',
+        'name' => 'Morto',
+        'cpf' => '287.852.723-29',
+        'img' => 'imagem/user4.png'
+    ],
+    'usuario5' => [
+        'User' => 'portugues@mail.com',
+        'pass' => '75',
+        'name' => 'ladrao',
+        'cpf' => '511.742.367-44',
+        'img' => 'imagem/user5.png'
+    ]
+] ;
